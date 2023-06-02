481826b0f5df64105239fadcf789ac6c3c62a4fd
VladaMalysheva <ik02.vlada.m@gmail.com>
2023-06-02 13:58:24 +0300

    did searching by jql query

--- src/main/java/com/example/proj/rest/MyRestResource.java
+++ src/main/java/com/example/proj/rest/MyRestResource.java
@@ -1,10 +1,15 @@
 package com.example.proj.rest;
 
+
 import com.atlassian.jira.bc.issue.search.SearchService;
+import com.atlassian.jira.issue.search.SearchException;
+import com.atlassian.jira.issue.search.SearchResults;
 import com.atlassian.jira.security.JiraAuthenticationContext;
 import com.atlassian.jira.user.ApplicationUser;
+import com.atlassian.jira.web.bean.PagerFilter;
 import com.atlassian.plugin.spring.scanner.annotation.imports.JiraImport;
 import com.atlassian.plugins.rest.common.security.AnonymousAllowed;
+import com.opensymphony.workflow.QueryNotSupportedException;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -22,6 +27,7 @@ public class MyRestResource {
     public MyRestResource(JiraAuthenticationContext authenticationContext, SearchService searchService) {
         this.authenticationContext = authenticationContext;
         this.searchService = searchService;
+
     }
 
     @JiraImport
@@ -29,15 +35,28 @@ public class MyRestResource {
 
 
 
-
     @GET
     @AnonymousAllowed
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    public Response getMessage(@QueryParam("URL") String URL, @QueryParam("jql") String jql, @QueryParam("fields")String fields)
+    public Response getMessage(@DefaultValue("project = TEST") @QueryParam("jql") String jql, @QueryParam("fields")String fields)
     {
-
         ApplicationUser user = authenticationContext.getLoggedInUser();
         SearchService.ParseResult parseResult = searchService.parseQuery(user, jql);
-        return Response.ok(new MyRestResourceModel("Hello World")).build();
+        try {
+
+        if(!parseResult.isValid())throw new QueryNotSupportedException();
+
+        parseResult = searchService.parseQuery(user, jql);
+
+        String username = user.getUsername();
+
+        SearchResults searchResults = searchService.search(user, parseResult.getQuery(), PagerFilter.getUnlimitedFilter());
+
+        return Response.ok(searchResults.getIssues()).build();
+
+        } catch (SearchException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
--- src/main/java/com/example/proj/rest/MyRestResourceModel.java
+++ src/main/java/com/example/proj/rest/MyRestResourceModel.java
@@ -1,18 +1,40 @@
 package com.example.proj.rest;
 
+import com.atlassian.jira.issue.Issue;
+import com.atlassian.plugin.spring.scanner.annotation.imports.JiraImport;
+
 import javax.xml.bind.annotation.*;
+import java.util.List;
+
 @XmlRootElement(name = "message")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class MyRestResourceModel {
 
+
     @XmlElement(name = "value")
     private String message;
+
+    @XmlElement(name = "issues")
+    private List<Issue> issues;
 
     public MyRestResourceModel() {
     }
 
+    public MyRestResourceModel(String message, List<Issue> issues) {
+        this.message = message;
+        this.issues = issues;
+    }
+
     public MyRestResourceModel(String message) {
         this.message = message;
+    }
+
+    public List<Issue> getIssues() {
+        return issues;
+    }
+
+    public void setIssues(List<Issue> issues) {
+        this.issues = issues;
     }
 
     public String getMessage() {
